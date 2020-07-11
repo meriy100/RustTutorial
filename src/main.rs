c70b3eba7b8778a894d5bba8ab31b0583af53bcc
meriy100 <ttattataa@gmail.com>
2020-07-11 14:02:17 +0900

    [Add] parse bf command

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,32 @@
-extern crate rand;
+use std::fs::File;
+use std::io::{self, Read};
 
-use std::io;
-//use std::cmp::Ordering;
-//use rand::Rng;
-
-fn code() -> String {
-    return ",>+<".to_string();
+#[derive(Debug, PartialEq)]
+enum CommandType {
+    Inc, Dec, PInc, PDec, Get, Set, While, End
 }
 
-fn main() {
-    let v: Vec<&str> = code().split("").collect();
-
-    for i in &v {
-        println!("{}", i);
+fn char_to_command_type(c: char) -> Option<CommandType> {
+    return match c {
+        '+' => Some(CommandType::Inc),
+        '-' => Some(CommandType::Dec),
+        '>' => Some(CommandType::PInc),
+        '<' => Some(CommandType::PDec),
+        '[' => Some(CommandType::While),
+        ']' => Some(CommandType::End),
+        '.' => Some(CommandType::Set),
+        ',' => Some(CommandType::Get),
+        _ => None
     }
+}
+
+fn main() -> io::Result<()> {
+    let mut file = File::open("test.bf")?;
+    let mut contents = String::new();
+    let mut memory: Vec<u8> = vec![0, 100];
+
+    file.read_to_string(&mut contents)?;
+    let v2: Vec<CommandType> = contents.chars().filter_map(char_to_command_type).collect::<Vec<CommandType>>();
+    println!("{:?}", v2);
+    Ok(())
 }
