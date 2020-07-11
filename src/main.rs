f987e94ca57cc8fdde308e28e1f067e049d6d0a5
meriy100 <ttattataa@gmail.com>
2020-07-11 22:47:50 +0900

    [Add] while ループでexecute

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,14 @@ enum CommandType {
 struct Node {
     command_type: CommandType,
     left: Option<Box<Node>>,
-    right: Option<Box<Node>>
+    right: Option<Box<Node>>,
 }
 
 #[derive(Debug, PartialEq)]
 struct Interpreter {
     memory: Vec<u8>,
-    pointer: usize
+    pointer: usize,
+    current_node: Option<Box<Node>>
 }
 fn char_to_command_type(c: char) -> Option<CommandType> {
     return match c {
@@ -77,87 +78,102 @@ fn parse(v2: &mut Vec<CommandType>) -> Option<Node> {
     }
 }
 
-fn execute(interpreter: &mut Interpreter, node: Node) -> io::Result<()> {
-    match node.command_type {
-        CommandType::Inc => {
-            interpreter.memory[interpreter.pointer] += 1;
-            match node.left {
-                Some(left) => {
-                    execute(interpreter, *left)
-                },
-                None => Ok(())
-            }
-        },
-        CommandType::Dec => {
-            interpreter.memory[interpreter.pointer] -= 1;
-            match node.left {
-                Some(left) => {
-                    execute(interpreter, *left)
-                },
-                None => Ok(())
-            }
-        },
-        CommandType::PInc => {
-            interpreter.pointer += 1;
-            match node.left {
-                Some(left) => {
-                    execute(interpreter, *left)
-                },
-                None => Ok(())
-            }
-        },
-        CommandType::PDec => {
-            interpreter.pointer -= 1;
-            match node.left {
-                Some(left) => {
-                    execute(interpreter, *left)
-                },
-                None => Ok(())
-            }
-        },
-        CommandType::While => {
-            if interpreter.memory[interpreter.pointer] == 0 {
-                match node.right {
-                    Some(right) => {
-                        execute(interpreter, *right)
-                    },
-                    None => Ok(())
-                }
-            } else {
-                match node.left {
-                    Some(left) => {
-                        execute(interpreter, *left)
-                    },
-                    None => Ok(())
-                }
-            }
-        }
-        _ => {
-            Ok(())
-        }
-    }
+// fn execute(interpreter: &mut Interpreter) -> io::Result<()> {
+//     match node.command_type {
+//         CommandType::Inc => {
+//             interpreter.memory[interpreter.pointer] += 1;
+//             match node.left {
+//                 Some(left) => {
+//                     execute(interpreter)
+//                 },
+//                 None => Ok(())
+//             }
+//         },
+//         CommandType::Dec => {
+//             interpreter.memory[interpreter.pointer] -= 1;
+//             match node.left {
+//                 Some(left) => {
+//                     execute(interpreter)
+//                 },
+//                 None => Ok(())
+//             }
+//         },
+//         CommandType::PInc => {
+//             interpreter.pointer += 1;
+//             match node.left {
+//                 Some(left) => {
+//                     execute(interpreter)
+//                 },
+//                 None => Ok(())
+//             }
+//         },
+//         CommandType::PDec => {
+//             interpreter.pointer -= 1;
+//             match node.left {
+//                 Some(left) => {
+//                     execute(interpreter)
+//                 },
+//                 None => Ok(())
+//             }
+//         },
+//         CommandType::While => {
+//             if interpreter.memory[interpreter.pointer] == 0 {
+//                 match node.right {
+//                     Some(right) => {
+//                         execute(interpreter)
+//                     },
+//                     None => Ok(())
+//                 }
+//             } else {
+//                 match node.left {
+//                     Some(left) => {
+//                         execute(interpreter)
+//                     },
+//                     None => Ok(())
+//                 }
+//             }
+//         }
+//         _ => {
+//             Ok(())
+//         }
+//     }
+// }
+
+
+fn execute(interpreter: Interpreter) -> Option<Interpreter> {
+    let mut memory = interpreter.memory;
+    let pointer = interpreter.pointer;
+     interpreter.current_node.and_then(|current_node|
+         match (*current_node).command_type {
+            CommandType::Inc => {
+                memory[pointer] += 1;
+                Some(Interpreter {
+                    memory,
+                    pointer: 0,
+                    current_node: (*current_node).left
+                })
+            },
+            _ => None
+         }
+     )
 }
 
 fn main() -> io::Result<()> {
     let mut file = File::open("test.bf")?;
     let mut contents = String::new();
-    let mut interpreter: Interpreter = Interpreter {
-        memory: vec![0; 100],
-        pointer: 0
-    };
 
     file.read_to_string(&mut contents)?;
     let mut v2: Vec<CommandType> = contents.chars().filter_map(char_to_command_type).collect::<Vec<CommandType>>();
     v2.reverse();
-    match parse(&mut v2) {
-        Some(ast) => {
-            println!("{:?}", ast);
-            execute(&mut interpreter, ast);
-        }
-        None => {
-
-        }
+    let mut optionInterpreter: Option<Interpreter> = parse(&mut v2).and_then(|ast|
+        Some(Interpreter {
+            memory: vec![0; 100],
+            pointer: 0,
+            current_node: Some(Box::new(ast))
+        })
+    );
+    while optionInterpreter.is_some() {
+        optionInterpreter = optionInterpreter.and_then(execute);
     }
-    println!("{:?}", interpreter.memory);
     Ok(())
 }
