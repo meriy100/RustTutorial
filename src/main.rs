0533c9c3d767f583996f2d66638cc4365b2e400d
meriy100 <ttattataa@gmail.com>
2020-07-11 17:58:18 +0900

    [Add] end get set 以外の excecute

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,11 @@ struct Node {
     right: Option<Box<Node>>
 }
 
+#[derive(Debug, PartialEq)]
+struct Interpreter {
+    memory: Vec<u8>,
+    pointer: usize
+}
 fn char_to_command_type(c: char) -> Option<CommandType> {
     return match c {
         '+' => Some(CommandType::Inc),
@@ -29,16 +34,42 @@ fn char_to_command_type(c: char) -> Option<CommandType> {
 
 fn parse(v2: &mut Vec<CommandType>) -> Option<Node> {
     return match v2.pop() {
-        Some(command_type) =>
+        Some(CommandType::While) =>
             Some(
                 Node {
-                    command_type: command_type,
+                    command_type: CommandType::While,
                     left: (
                             match parse(v2) {
                                 Some(node) => Some(Box::new(node)),
                                 None => None
                             }
                         ),
+                    right: (
+                            match parse(v2) {
+                                Some(node) => Some(Box::new(node)),
+                                None => None
+                            }
+                        )
+                }
+            ),
+        Some(CommandType::End) =>
+            Some(
+                Node {
+                    command_type: CommandType::End,
+                    left: None,
+                    right: None
+                }
+            ),
+        Some(command_type) =>
+            Some(
+                Node {
+                    command_type,
+                    left: (
+                        match parse(v2) {
+                            Some(node) => Some(Box::new(node)),
+                            None => None
+                        }
+                    ),
                     right: None
                 }
             ),
@@ -46,28 +77,87 @@ fn parse(v2: &mut Vec<CommandType>) -> Option<Node> {
     }
 }
 
+fn execute(interpreter: &mut Interpreter, node: Node) -> io::Result<()> {
+    match node.command_type {
+        CommandType::Inc => {
+            interpreter.memory[interpreter.pointer] += 1;
+            match node.left {
+                Some(left) => {
+                    execute(interpreter, *left)
+                },
+                None => Ok(())
+            }
+        },
+        CommandType::Dec => {
+            interpreter.memory[interpreter.pointer] -= 1;
+            match node.left {
+                Some(left) => {
+                    execute(interpreter, *left)
+                },
+                None => Ok(())
+            }
+        },
+        CommandType::PInc => {
+            interpreter.pointer += 1;
+            match node.left {
+                Some(left) => {
+                    execute(interpreter, *left)
+                },
+                None => Ok(())
+            }
+        },
+        CommandType::PDec => {
+            interpreter.pointer -= 1;
+            match node.left {
+                Some(left) => {
+                    execute(interpreter, *left)
+                },
+                None => Ok(())
+            }
+        },
+        CommandType::While => {
+            if interpreter.memory[interpreter.pointer] == 0 {
+                match node.right {
+                    Some(right) => {
+                        execute(interpreter, *right)
+                    },
+                    None => Ok(())
+                }
+            } else {
+                match node.left {
+                    Some(left) => {
+                        execute(interpreter, *left)
+                    },
+                    None => Ok(())
+                }
+            }
+        }
+        _ => {
+            Ok(())
+        }
+    }
+}
+
 fn main() -> io::Result<()> {
     let mut file = File::open("test.bf")?;
     let mut contents = String::new();
-    let mut memory: Vec<u8> = vec![0; 100];
+    let mut interpreter: Interpreter = Interpreter {
+        memory: vec![0; 100],
+        pointer: 0
+    };
 
     file.read_to_string(&mut contents)?;
     let mut v2: Vec<CommandType> = contents.chars().filter_map(char_to_command_type).collect::<Vec<CommandType>>();
     v2.reverse();
-    // match v2.pop() {
-    //     Some(command_type) => {
-    //        let mut ast: Node = Node { command_type: command_type, left: None, right: None };
-    //         match v2.pop() {
-    //             Some(command_type1) => {
-    //                 ast.left = Some(Box::new(Node { command_type: command_type1, left: None, right: None }));
-    //
-    //             },
-    //             None => {}
-    //         }
-    //     },
-    //     None => {}
-    // }
-    println!("{:?}", parse(&mut v2));
-    println!("{:?}", memory);
+    match parse(&mut v2) {
+        Some(ast) => {
+            println!("{:?}", ast);
+            execute(&mut interpreter, ast);
+        }
+        None => {
+
+        }
+    }
+    println!("{:?}", interpreter.memory);
     Ok(())
 }
